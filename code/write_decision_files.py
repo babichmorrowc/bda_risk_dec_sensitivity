978ce18c4a920733695a629b72b40751f6b3fa94
babichmorrowc <babichmorrowc@gmail.com>
2025-01-07 13:36:59 +0000

    Plot percent optimal

--- code/write_decision_files.py
+++ code/write_decision_files.py
@@ -121,10 +121,71 @@ lat_land = data['lat']
 ax1 = plt.subplot(1,1,1,projection=ccrs.PlateCarree())
 ax1.set_xlabel('Longitude')
 ax1.set_ylabel('Latitude')
-cols = ListedColormap(['blue','orange','yellow'])
+cols = ListedColormap(['blue',
+                    #    'orange',
+                       'yellow'])
 norm = BoundaryNorm([1,2,3], cols.N)
-classes = ['1 optimal decision','2 optimal decisions','3 optimal decisions']
+classes = ['1 optimal decision',
+        #    '2 optimal decisions',
+           '3 optimal decisions']
 scatter = ax1.scatter(lon_land,lat_land,c=noptions,s=12,cmap=cols,norm=norm)
 ax1.legend(handles=scatter.legend_elements()[0], labels=classes)
 ax1.coastlines()
+plt.show()
+
+# Plot percentage of time that each decision was optimal in a cell
+
+# Calculate frequencies of each decision per location
+column_frequencies = {value: [] for value in decision_options[0]}
+num_rows = Y.shape[0]
+
+for col_index in range(Y.shape[1]):
+    unique_values, value_counts = np.unique(Y[:, col_index], return_counts=True)
+    # Create a dictionary to hold frequency counts for current column
+    freq_dict = dict(zip(unique_values, value_counts))
+    # Calculate frequencies for each decision option
+    for value in decision_options[0]:
+        if value in freq_dict:
+            frequency = freq_dict[value] / num_rows
+        else:
+            frequency = 0.0  # Value not present, set frequency to 0
+        
+        column_frequencies[value].append(frequency)
+
+frequencies_df = pd.DataFrame(column_frequencies)
+
+fig = plt.figure(figsize=(18,9))
+ax1 = plt.subplot(1,3,1,projection=ccrs.PlateCarree())
+ax2 = plt.subplot(1,3,2,projection=ccrs.PlateCarree())
+ax3 = plt.subplot(1,3,3,projection=ccrs.PlateCarree())
+lon_land = data['lon']
+lat_land = data['lat']
+
+# Percent of time that decision 1 is optimal
+ax1.set_xlabel('Longitude')
+ax1.set_ylabel('Latitude')
+scatter = ax1.scatter(lon_land,lat_land,c=frequencies_df[1],s=5,vmin=0,vmax=1,cmap='Greens')
+cbar = plt.colorbar(scatter,ax=ax1,shrink=0.3)
+cbar.set_label('% samples optimal')
+ax1.coastlines()
+ax1.title.set_text('Decision 1: % samples optimal')
+
+# Percent of time that decision 2 is optimal
+ax2.set_xlabel('Longitude')
+ax2.set_ylabel('Latitude')
+scatter = ax2.scatter(lon_land,lat_land,c=frequencies_df[2],s=5,vmin=0,vmax=1,cmap='Greens')
+cbar = plt.colorbar(scatter,ax=ax2,shrink=0.3)
+cbar.set_label('% samples optimal')
+ax2.coastlines()
+ax2.title.set_text('Decision 2: % samples optimal')
+
+# Percent of time that decision 3 is optimal
+ax3.set_xlabel('Longitude')
+ax3.set_ylabel('Latitude')
+scatter = ax3.scatter(lon_land,lat_land,c=frequencies_df[3],s=5,vmin=0,vmax=1,cmap='Greens')
+cbar = plt.colorbar(scatter,ax=ax3,shrink=0.3)
+cbar.set_label('% samples optimal')
+ax3.coastlines()
+ax3.title.set_text('Decision 3: % samples optimal')
+
 plt.show()
